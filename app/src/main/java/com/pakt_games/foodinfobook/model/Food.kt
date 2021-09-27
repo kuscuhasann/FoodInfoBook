9c8ec6fc424c92b8db1cb17528b13f23870c5cdd
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-09-27 20:49:50 +0300

    Added Primary Key for ID

--- app/src/main/java/com/pakt_games/foodinfobook/model/Food.kt
+++ app/src/main/java/com/pakt_games/foodinfobook/model/Food.kt
@@ -2,27 +2,31 @@ package com.pakt_games.foodinfobook.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 @Entity
 data class Food(
     @ColumnInfo(name = "isim")
     @SerializedName("isim")
-    val foodName:String?,
+    val foodName : String?,
     @ColumnInfo(name = "kalori")
     @SerializedName("kalori")
-    val foodCalorie:String?,
+    val foodCalorie : String?,
     @ColumnInfo(name = "karbonhidrat")
     @SerializedName("karbonhidrat")
-    val foodCarbohydrate:String?,
+    val foodCarbohydrate : String?,
     @ColumnInfo(name = "protein")
     @SerializedName("protein")
-    val foodProtein:String?,
+    val foodProtein : String?,
     @ColumnInfo(name = "yag")
     @SerializedName("yag")
-    val foodFat:String?,
+    val foodFat : String?,
     @ColumnInfo(name = "gorsel")
     @SerializedName("gorsel")
-    val foodImage:String?
-
+    val foodImage : String?
 )
+{
+    @PrimaryKey(autoGenerate = true)
+    val uuid : Int = 0
+}
