ce52b41e81cb79844a26f806755db115cc2fa10f
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-09-27 21:28:25 +0300

    Creaated DAO Interface with using Coroutines for ROOM

---
+++ app/src/main/java/com/pakt_games/foodinfobook/dao/FoodDAO.kt
@@ -0,0 +1,18 @@
+package com.pakt_games.foodinfobook.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.pakt_games.foodinfobook.model.Food
+
+@Dao
+interface FoodDAO {
+    @Insert
+    suspend fun insertAll(vararg food:Food) : List<Long>
+    @Query("SELECT * FROM FOOD")
+    suspend fun getAllFoods() : List<Food>
+    @Query("SELECT * FROM FOOD WHERE uuid = :FoodId")
+    suspend fun getFood(FoodId : Int) : Food
+    @Query("DELETE FROM FOOD")
+    suspend fun deleteAllFoods()
+}
