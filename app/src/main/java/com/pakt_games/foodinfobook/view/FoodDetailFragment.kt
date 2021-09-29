27691d736f6b8035e6ba7cea4d8c5a878225db4d
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-09-30 01:55:00 +0300

    DataBinding Implementation Complated

--- app/src/main/java/com/pakt_games/foodinfobook/view/FoodDetailFragment.kt
+++ app/src/main/java/com/pakt_games/foodinfobook/view/FoodDetailFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.pakt_games.foodinfobook.R
+import com.pakt_games.foodinfobook.databinding.FragmentFoodDetailBinding
 import com.pakt_games.foodinfobook.util.createPlaceholder
 import com.pakt_games.foodinfobook.util.downloadImage
 import com.pakt_games.foodinfobook.viewmodel.FoodDetailViewModel
@@ -17,6 +19,7 @@ import kotlinx.android.synthetic.main.fragment_food_detail.*
 class FoodDetailFragment : Fragment() {
     private  lateinit var viewModel: FoodDetailViewModel
     private var foodId=0
+    private lateinit var databinding: FragmentFoodDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -27,7 +30,8 @@ class FoodDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food_detail, container, false)
+        databinding=DataBindingUtil.inflate(inflater,R.layout.fragment_food_detail,container,false)
+        return databinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -46,16 +50,7 @@ class FoodDetailFragment : Fragment() {
     private fun observeLiveData() {
         viewModel.foodLiveData.observe(viewLifecycleOwner, Observer {food->
             food?.let {
-                //İmage
-                txtFoodNameIdAtFoodDetail.text=it.foodName
-                txtFoodCalorieIdAtFoodDetail.text=it.foodCalorie
-                txtFoodProteinIdAtFoodDetail.text=it.foodProtein
-                txtFoodFatIdAtFoodDetail.text=it.foodFat
-                txtFoodCarbohydrateIdAtFoodDetail.text=it.foodCarbohydrate
-                context?.let {
-                    foodImageViewIdAtFoodDetail.downloadImage(food.foodImage, createPlaceholder(it))
-                }
-
+                databinding.model=it
             }
         })
     }
